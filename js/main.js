0edf3ab5e5393670d47bfc3f3056ac3f0cb445ef
svr8 <shikhar.vaish90@gmail.com>
2019-06-13 01:11:30 +0530

    activated enter key on form submission

--- js/main.js
+++ js/main.js
@@ -3,3 +3,13 @@ var URL = "http://ccpredictor-ccrate.apps.us-east-2.online-starter.openshift.com
 document.getElementById("predictButton").onclick = function() {
     window.location = URL + document.getElementById("contestCode").value + "/all"
 }
+
+$(document).ready(function(){
+  $("#contestCode").on('keyup keypress', function(event){
+    console.log(event.keyCode);
+    if(event.keyCode == 13) {
+      event.preventDefault();      
+      $("#predictButton").trigger("click");
+    }
+  })
+})
