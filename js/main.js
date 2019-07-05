ef706626aaa24788d7be5300f5e54a196b3507dd
dush1729 <dush1729@gmail.com>
2019-07-06 00:31:02 +0530

    Use Heroku instead of OpenShift

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var URL = "http://ccpredictor-ccrate.apps.us-east-2.online-starter.openshift.com/contest/"
+var URL = "http://ccpredict.herokuapp.com/contest/"
 
 document.getElementById("predictButton").onclick = function() {
     window.location = URL + document.getElementById("contestCode").value + "/all"
